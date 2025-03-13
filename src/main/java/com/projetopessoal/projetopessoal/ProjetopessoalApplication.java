b1a8d08f736a41216665a6485af2e586e71ca1df
Thiago Augusto <155887868+ThiagoAugusto0@users.noreply.github.com>
2025-03-13 18:13:22 -0300

    atualizando projeto

---
+++ src/main/java/com/projetopessoal/projetopessoal/ProjetopessoalApplication.java
@@ -0,0 +1,14 @@
+package com.projetopessoal.projetopessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjetopessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetopessoal/projetopessoal/ProjetopessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetopessoal.projetopessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
